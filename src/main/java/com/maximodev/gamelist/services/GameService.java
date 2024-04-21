53ee2a1f223ca4972c69cda48353a920a08663a3
Felipe Maximo <felipecontato2001@hotmail.com>
2024-04-21 07:28:19 -0300

    criando game service, repository, controller e seu DTO

---
+++ src/main/java/com/maximodev/gamelist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.maximodev.gamelist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maximodev.gamelist.dto.GameMinDTO;
+import com.maximodev.gamelist.entities.Game;
+import com.maximodev.gamelist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll(){
+        var result = gameRepository.findAll();
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+
+}
