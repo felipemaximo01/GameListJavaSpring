de487f0c1955b5e0804b8b3bcd62b5b01ee4384e
Felipe Maximo <felipecontato2001@hotmail.com>
2024-04-21 23:54:58 -0300

    implementando gameById

--- src/main/java/com/maximodev/gamelist/services/GameService.java
+++ src/main/java/com/maximodev/gamelist/services/GameService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.maximodev.gamelist.dto.GameDTO;
 import com.maximodev.gamelist.dto.GameMinDTO;
+import com.maximodev.gamelist.entities.Game;
 import com.maximodev.gamelist.repositories.GameRepository;
 
 @Service
@@ -14,6 +17,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game game = gameRepository.findById(id).get();
+        return new GameDTO(game);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         var result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
